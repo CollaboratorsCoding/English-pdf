4bba9988e084cd125be085be87e9f3fcaedb038d
Andrew Tomash <tomasz9201@gmail.com>
2019-06-02 14:28:47 +0300

    test

--- server/api/index.js
+++ server/api/index.js
@@ -3,9 +3,7 @@ const express = require('express');
 const api = express.Router();
 
 api.get('/test', (req, res) => {
-	return res.json({
-		test: 'test api'
-	})
+				return res.json({test: 'test api'})
 })
 
 module.exports = api;
